a7ad1edacb7eaea7e041bcf7878157952d088acb
devilmambr <154016885+devilmambr@users.noreply.github.com>
2024-10-10 09:24:23 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'basquete',
+    'Quem tem o melhor percentual de tocos na temporada 23-24 na nba? ',
+    '|Victor  Wembanyama'
+)
+
+criaCartao(
+    'jogo',
+    'Quem é o caçador do vazio?',
+    'Kassadin'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual é cor que é todas as cores juntas?',
+    'Branca'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual é o jogador que receber mais?',
+    'cristiano ronaldo'
+)
